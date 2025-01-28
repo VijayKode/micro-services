38dbc3af3742b2b541d0b34621b42b00577db889
Vijaysambasivarao Kode <vijaysambasivarao_kode@epam.com>
2025-01-28 20:48:24 +0530

    added micro services patterns

---
+++ designpatterns/apigatewaypattern/src/main/java/practice/micoservices/apigatewaypattern/ApigatewaypatternApplication.java
@@ -0,0 +1,13 @@
+package practice.micoservices.apigatewaypattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewaypatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewaypatternApplication.class, args);
+	}
+
+}
---
+++ designpatterns/apigatewaypattern/src/test/java/practice/micoservices/apigatewaypattern/ApigatewaypatternApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.micoservices.apigatewaypattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewaypatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
